2a81e996eab5647b5fc000bfe5950229b9f48ed4
Daniil <70716111+DragonLorde@users.noreply.github.com>
2022-06-08 16:29:04 +0500

    Update Employers.schema.ts

--- src/model/Employers.schema.ts
+++ src/model/Employers.schema.ts
@@ -5,6 +5,10 @@ const employersSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+   jobTitle: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
